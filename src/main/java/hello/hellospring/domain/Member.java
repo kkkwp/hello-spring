361f04e1ef6a1ff57923f3926289543e0869e641
kkkwp <chxrryda@gmail.com>
2022-04-03 19:39:59 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity // ORM mapping
 public class Member {
 
+//  pk
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 ID를 자동 생성하는 전략
     private Long id;
+
+//  @Column(name = "username")  // 만약 name이 DB에 username이라는 컬럼으로 저장될 경우
     private String name;
 
     public Long getId() {
